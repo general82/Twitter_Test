8445afe2362dea400228cd19e66e5940dd649d69
general82 <maximov@bk.ru>
2017-12-25 21:02:09 +0700

    TwitterTest

---
+++ TwitterTest/TwitterTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterTest
+//
+//  Created by Konstantin on 25.12.17.
+//  Copyright © 2017 KONSTATIN MAXIMOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterTest/TwitterTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwitterTest
+//
+//  Created by Konstantin on 25.12.17.
+//  Copyright © 2017 KONSTATIN MAXIMOV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
